4f97fa5fe555383368cb0d6aeaad5430fb55abaf
nagag08 <110815496+nagag08@users.noreply.github.com>
2024-06-08 18:18:06 +0530

    Update createpolicy.sh

--- xray/policy/createpolicy.sh
+++ xray/policy/createpolicy.sh
@@ -1,1 +1,1 @@
-curl curl -H "Authorization: Bearer $TOKEN" -XPOST api/v2/policies -H "Content-Type: application/json"  -T policy.json
+curl curl -H "Authorization: Bearer $TOKEN" -XPOST https://<arturl>/xray/api/v2/policies -H "Content-Type: application/json"  -T policy.json
